b810c4d57ffb6f8998d54cdfc52cb9b9f009c9ef
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-09-27 16:16:09 -0300

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Bruno Oliveira on 27/09/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
